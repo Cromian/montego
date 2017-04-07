88f8c0b48cbe2df5a697f651e1de61579d675519
Misha <Cromian@users.noreply.github.com>
2017-04-06 20:49:38 -0400

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,5 @@
 <footer>
 
-	<div class="campaign-legal-box">Proudly created by David For Delegate volunteers.</div>
-
 	<div class="legal">&copy;<span id="footer-date"></span> <? php include($site_title); ?></div>
 
 </footer>
