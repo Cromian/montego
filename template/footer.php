cb603a12aabc332c49d61e2300c7cbf739e40f28
Misha <Cromian@users.noreply.github.com>
2017-04-06 20:49:23 -0400

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -2,6 +2,6 @@
 
 	<div class="campaign-legal-box">Proudly created by David For Delegate volunteers.</div>
 
-	<div class="legal">&copy;<span id="footer-date"></span> David Rose-Carmack, Candidate for Delegate.</div>
+	<div class="legal">&copy;<span id="footer-date"></span> <? php include($site_title); ?></div>
 
 </footer>
