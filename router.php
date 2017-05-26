54f906c74f0e731c5e1873430991fc47defdaa5f
Misha <misha@workbookmv.local>
2017-05-26 05:32:14 -0400

    re-worekd pulgins into components

--- router.php
+++ router.php
@@ -1,25 +1,25 @@
 <?php
-// Template Objects
+// Template: Objects
 $header = "template/header.php";
 $footer = "template/footer.php";
 $navigation = "template/navigation.php";
 $meta_top = "template/meta/meta-top.php";
 $meta_bot = "template/meta/meta-bot.php";
 
-// Assets - OpenGraph
+// Template: Components
+$email_form = 'template/components/email.php';
+$rss_reader = 'template/components/rss-reader.php';
+$ga_analytics = 'template/components/ga_analytics.php';
+
+// Assets: OpenGraph
 $og_fb_img = 'assets/og/facebook.png';
 $og_twitter_img = 'assets/og/twitter.png';
 
-// Assets - CSS
+// Assets: CSS
 $asset_css = 'assets/main.css';
 
-// Assets - JS
+// Assets: JS
 $asset_js = 'assets/scripts.js';
 $jquery_url = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js';
-
-// Plugins
-$email_form = 'plugins/email.php';
-$rss_reader = 'plugins/rss-reader.php';
-$ga_analytics = 'plugins/ga_analytics.php';
 
 ?>
